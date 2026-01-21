b70b592ccebc90adbd84a70173632d1bbcd4ad06
luojingwei <jonnylaw89@163.com>
2026-01-21 09:45:12 +0800

    feat: 添加预训练和微调指令

--- command.py
+++ command.py
@@ -2,19 +2,46 @@ import subprocess
 import time
 
 # 指令列表
-commands = [
-    f"CUDA_VISIBLE_DEVICES=1,2,3,4,5,6,7,8 python pretrain_main.py --dataset_dir /data1/hust_bciml_eegdata/lmdb/TUEG-lmdb/ --epochs 200" # 预训练指令
+model_dir = "pretrained_weights.pth" # epoch197_loss0.001582802040502429.pth  pretrained_weights.pth epoch999_loss0.0014723502099514008.pth
+seed = 3016
+classifier = "all_patch_reps" # all_patch_reps, avgpooling_patch_reps
+devices = "0,1,2,3"
+data_root_dir = "/data1/hust_bciml_eegdata/lmdb"
 
-    # 微调PhysioNet-MI指令
-    f"CUDA_VISIBLE_DEVICES=1,2,3,4,5,6,7,8 \
-    python finetune_main.py --foundation_dir pretrained_weights/epoch39_loss0.0030975404661148787.pth \
-    --downstream_dataset PhysioNet-MI --datasets_dir /data1/hust_bciml_eegdata/lmdb/PhysioNetMI-lmdb/ \
-    --num_of_classes 4 --classifier all_patch_reps \
-    --seed 997"
+commands = [
+    # f"/usr/local/anaconda3/envs/labram/bin/python pretrain_main.py --dataset_dir {data_root_dir}/TUEG-lmdb/ --epochs 1000" # 预训练指令
+
+    # # 微调PhysioNet-MI指令
+    # f"CUDA_VISIBLE_DEVICES={devices} \
+    # python finetune_main.py --foundation_dir pretrained_weights/{model_dir} \
+    # --downstream_dataset PhysioNet-MI --datasets_dir {data_root_dir}/PhysioNetMI-lmdb/ \
+    # --num_of_classes 4 --classifier {classifier} \
+    # --seed {seed}",
+
+    # # 微调FACED指令
+    # f"CUDA_VISIBLE_DEVICES={devices} \
+    # python finetune_main.py --foundation_dir pretrained_weights/{model_dir} \
+    # --downstream_dataset FACED --datasets_dir {data_root_dir}/FACED-lmdb/ \
+    # --num_of_classes 9 --classifier {classifier} \
+    # --seed {seed}",
+
+    # # 微调BCIC2020-3指令
+    # f"CUDA_VISIBLE_DEVICES={devices} \
+    # python finetune_main.py --foundation_dir pretrained_weights/{model_dir} \
+    # --downstream_dataset BCIC2020-3 --datasets_dir {data_root_dir}/BCIC2020-3-lmdb/ \
+    # --num_of_classes 5 --classifier {classifier} \
+    # --seed {seed}",
+
+    # # 微调BCIC-IV-2a指令
+    # f"CUDA_VISIBLE_DEVICES={devices} \
+    # python finetune_main.py --foundation_dir pretrained_weights/{model_dir} \
+    # --downstream_dataset BCIC-IV-2a --datasets_dir {data_root_dir}/BCICIV-2a-lmdb/ \
+    # --num_of_classes 4 --classifier {classifier} \
+    # --seed {seed}",
 ]
 
 # 重试次数设置
-max_retries = 3
+max_retries = 1
 
 # 依次执行每条指令
 for cmd in commands:
